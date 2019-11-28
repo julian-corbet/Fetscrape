c120f4757fefa24e5bb5306a009d0f9080e95136
M. Pomar <58166205+mpomar@users.noreply.github.com>
2019-11-28 21:51:20 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,44 @@
+﻿# Fetscrape
+
+The script helps you scraping users from FetLife, the most popular BDSM-related social network. Basically, it crawls the users of a certain area and export them to a .csv file.
+
+
+## Project Status
+
+Currently (November 2019) the script works like a charm. I will not maintain the code over time though. Should John change the HTML structure, the script will need to be updated accordingly. Feel free step in as a maintainer.
+
+
+## Description
+
+Before running the script you will need to insert your FL credentials (username and password) under 'login_data'. Please, do not change the other parameters.
+By default, when running the script, the list of the users located in Berlin, Germany will be captured:
+
+```bash
+https://fetlife.com/p/germany/berlin/kinksters?page=1
+```
+
+Of course, it is possible to change the targeted area. All you have to do is:
+1) Browse the desired location in FetLife
+2) Copy the URL of that location 
+3) Replace the URL in 'r2'
+
+Pay attention to the paging or it will return the results of the first page only!
+
+For each user you are going to store in the .csv file the following information:
+
+- User ID (for later reference; also useful if you are planning to load the users in a target system and would like to prevent the creation of duplicates)
+- Username 
+- Age / Gender (for simplicity I did not split them: I know, I am lazy, but you can still filter the results using "contains")
+- Role
+- Location
+- Extras (number of pictures / videos / posts)
+- Link to the profile page
+
+
+## Final comments
+Please keep in mind I am new to Python and I do not work as a programmer: I am pretty sure it would have been possible to write a better code... But, hey, in the end mine works too and that's what really counts! :-D
+If you found this script useful, don't forget to show some love to @Objectivist on FetLife :-D
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ fetscrape.py
@@ -0,0 +1,70 @@
+__author__ = "Manfredi Pomar"
+__email__ = "m.pomar@outlook.com"
+
+#module import#
+import requests
+from bs4 import BeautifulSoup
+import csv
+import time
+
+#prepare csv#
+exportcsv = open('Fetscrape.csv', 'w', newline='')
+csvwriter = csv.writer(exportcsv)
+csvwriter.writerow(['User ID', 'Username', 'Age / Gender', 'Role', 'Location', 'Extras', 'Link'])
+
+headers = {'user-agent': """Mozilla/5.0 (X11; Linux x86_64) """
+                         """AppleWebKit/537.36 (KHTML, like Gecko) """
+                         """Chrome/58.0.3029.110 Safari/537.36"""}
+#login#
+login_data = {
+    'user[login]': '',
+    'user[password]': '',
+    'user[locale]': 'en',
+    'user[otp_attempt]': 'step_1',
+    'utf8': '✓'
+}
+
+with requests.Session() as s:
+    url = 'https://fetlife.com/users/sign_in'
+    r = s.get(url, headers=headers)
+    soup = BeautifulSoup(r.content, 'lxml')
+    login_data['authenticity_token'] = soup.find('input', attrs={'name': 'authenticity_token'})['value']
+    r = s.post(url, data=login_data, headers=headers)
+
+#target definition#
+for i in range(10):
+    r2 = s.get('https://fetlife.com/p/germany/berlin/kinksters?page={}'.format(i+1), headers=headers)
+    soup = BeautifulSoup(r2.content, 'lxml')
+
+    #scrape data#
+    for results in soup.find_all('div', class_ ='w-100 br1 pointer bg-animate hover-bg-dark-primary bg-near-black'):
+        userID = results.find('a', {"class": 'link span f5 fw7 secondary'})
+        ID = (userID['href'])
+        ID = ID.split('/')[2]
+        print(ID)
+        nickname = userID.text
+        print(nickname)
+        details = results.find('span', {"class": 'f6 fw7 silver'}).text
+        age_gender = details.split(' ')[0]
+        role = details.split(' ')[1]
+        print(age_gender)
+        print(role)
+        place = results.find('div', {"class": 'f6 lh-copy fw4 silver nowrap truncate'}).text
+        place = place.replace(',', '')
+        print(place)
+        extras = results.find('div', {"class": 'relative pd1 f6 fw4 lh-copy mid-gray nowrap truncate'}).text
+        extras = extras.replace(',', '')
+        extras = extras.replace('·', '')
+        print(extras)
+        link = f"https://fetlife.com/users/{ID}"
+        print(link)
+
+        print()
+
+        # export to csv#
+        csvwriter.writerow([ID, nickname, age_gender, role, place, extras, link])
+
+        time.sleep(1)
+
+
+exportcsv.close()
